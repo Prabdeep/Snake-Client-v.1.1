927fff38497f896b223e014955d73b7b990c35ad
Prabdeep <Prabdeepsingh@hotmail.com>
2015-11-17 10:48:55 +0100

    I have created the "Screen" class for the snake application by using java swing in Eclipse. I have further more created getters and setters for all the variables in Elicpse.
    // Prabdeep

--- src/GUI/Screen.java
+++ src/GUI/Screen.java
@@ -1,7 +1,104 @@
 package GUI;
 
-/**
- * Created by Prabdeep on 11-11-2015.
- */
-public class Screen {
+import java.awt.BorderLayout;
+import java.awt.CardLayout;
+import java.awt.EventQueue;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import GUI.Login;
+
+import java.awt.Toolkit;
+
+public class Screen extends JFrame {
+    public static final String LOGIN = "Login";
+    public static final String SNAKEMENU = "Snakemenu";
+    public static final String STARTGAME = "Startgame";
+    public static final String CREATEGAME = "Creategame";
+    public static final String DELETEGAME = "Deletegame";
+    public static final String LOADRESULT = "Loadresult";
+    public static final String LOADHIGHSCORES = "Loadhighscores";
+
+    private JPanel contentPane;
+
+    private Login login;
+    private SnakeMenu snakemenu;
+    private StartGame startgame;
+    private CreateGame creategame;
+    private DeleteGame deletegame;
+    private LoadResult loadresult;
+    private LoadHighscores loadhighscores;
+
+    private CardLayout c;
+
+    public Screen() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 450, 550);
+        contentPane = new JPanel();
+        contentPane.setLayout(new CardLayout());
+        setContentPane(contentPane);
+
+        login = new Login();
+        contentPane.add(login, LOGIN);
+
+        snakemenu = new SnakeMenu();
+        contentPane.add(snakemenu, SNAKEMENU);
+
+        startgame = new StartGame();
+        contentPane.add(startgame, STARTGAME);
+
+        creategame = new CreateGame();
+        contentPane.add(creategame, CREATEGAME);
+
+        deletegame = new DeleteGame();
+        contentPane.add(deletegame, DELETEGAME);
+
+        loadresult = new LoadResult();
+        contentPane.add(loadresult, LOADRESULT);
+
+        loadhighscores = new LoadHighscores();
+        contentPane.add(loadhighscores, LOADHIGHSCORES);
+
+
+        c = (CardLayout) getContentPane().getLayout();
+
+    }
+
+    public void show(String panel) {
+        c.show(contentPane, panel);
+    }
+
+    public Login getLogin() {
+        return login;
+    }
+
+    public SnakeMenu getSnakemenu() {
+        return snakemenu;
+    }
+
+    public StartGame getStartgame() {
+        return startgame;
+    }
+
+    public CreateGame getCreategame() {
+        return creategame;
+    }
+
+    public DeleteGame getDeletegame() {
+        return deletegame;
+    }
+
+    public LoadResult getLoadresult() {
+        return loadresult;
+    }
+
+    public LoadHighscores getLoadhighscores() {
+        return loadhighscores;
+    }
+
 }
