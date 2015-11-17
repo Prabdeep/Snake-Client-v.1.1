fd0dff66f3e8f2bbfc45164f68a5161a1d035466
Prabdeep <Prabdeepsingh@hotmail.com>
2015-11-17 14:35:38 +0100

    I have implemented som of the SDK example code from the hackathon.
    // Prabdeep

---
+++ src/SDK/Game.java
@@ -0,0 +1,63 @@
+package SDK;
+
+import java.util.Date;
+
+public class Game {
+
+    private int gameId;
+    private int result;
+    private int newGame;
+    private int endGame;
+    private String name;
+    private String hostControls;
+    private Players host;
+    private Players opponent;
+    private String opponentControls;
+    private int status;
+    private Date created;
+
+    public void setGameId(int gameId) {
+        this.gameId = gameId;
+    }
+
+    public void setResult(int result) {
+        this.result = result;
+    }
+
+    public void setNewGame(int newGame) {
+        this.newGame = newGame;
+    }
+
+    public void setEndGame(int endGame) {
+        this.endGame = endGame;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setHostControls(String hostControls) {
+        this.hostControls = hostControls;
+    }
+
+    public void setHost(Players host) {
+        this.host = host;
+    }
+
+    public void setOpponent(Players opponent) {
+        this.opponent = opponent;
+    }
+
+    public void setOpponentControls(String opponentControls) {
+        this.opponentControls = opponentControls;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+}
